0c5df15598e76145794bc58fba4fb5f3a910091f
Aryaman Singh <arya.ribharya@gmail.com>
2020-05-15 08:00:50 +0530

    initial commit

--- lib/coin_data.dart
+++ lib/coin_data.dart
@@ -1,4 +1,6 @@
 //TODO: Add your imports here.
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 const List<String> currenciesList = [
   'AUD',
@@ -34,5 +36,49 @@ const coinAPIURL = 'https://rest.coinapi.io/v1/exchangerate';
 const apiKey = 'YOUR-API-KEY-HERE';
 
 class CoinData {
+  CoinData(this.currency);
+
+
+  String currency;
+  String rate='';
+  Future <String> getCoinData ()
+  async {
+    var url ='https://rest.coinapi.io/v1/exchangerate/BTC/$currency?apikey=9101A53E-5CA9-43DD-987C-879EB946138B';
+    var response = await http.get(url);
+    String data=response.body;
+     rate=jsonDecode(data)['rate'].round().toString();
+    print(rate);
+    print(data);
+    return rate;
+
+
+
+  }
+  Future <String> getCoinData1 ()
+  async {
+    var url ='https://rest.coinapi.io/v1/exchangerate/ETH/$currency?apikey=9101A53E-5CA9-43DD-987C-879EB946138B';
+    var response = await http.get(url);
+    String data=response.body;
+    rate=jsonDecode(data)['rate'].round().toString();
+    print(rate);
+    print(data);
+    return rate;
+
+
+
+  }
+  Future <String> getCoinData2 ()
+  async {
+    var url ='https://rest.coinapi.io/v1/exchangerate/LTC/$currency?apikey=9101A53E-5CA9-43DD-987C-879EB946138B';
+    var response = await http.get(url);
+    String data=response.body;
+    rate=jsonDecode(data)['rate'].round().toString();
+    print(rate);
+    print(data);
+    return rate;
+
+
+
+  }
   //TODO: Create your getCoinData() method here.
 }
--- lib/price_screen.dart
+++ lib/price_screen.dart
@@ -3,6 +3,9 @@ import 'package:flutter/cupertino.dart';
 import 'coin_data.dart';
 import 'dart:io' show Platform;
 
+import 'coin_data.dart';
+import 'coin_data.dart';
+
 class PriceScreen extends StatefulWidget {
   @override
   _PriceScreenState createState() => _PriceScreenState();
@@ -10,6 +13,7 @@ class PriceScreen extends StatefulWidget {
 
 class _PriceScreenState extends State<PriceScreen> {
   String selectedCurrency = 'USD';
+
 
   DropdownButton<String> androidDropdown() {
     List<DropdownMenuItem<String>> dropdownItems = [];
@@ -27,6 +31,11 @@ class _PriceScreenState extends State<PriceScreen> {
       onChanged: (value) {
         setState(() {
           selectedCurrency = value;
+          getData(selectedCurrency);
+          getData1(selectedCurrency);
+          getData2(selectedCurrency);
+
+
         });
       },
     );
@@ -47,56 +56,137 @@ class _PriceScreenState extends State<PriceScreen> {
       children: pickerItems,
     );
   }
-
-  //TODO: Create a method here called getData() to get the coin data from coin_data.dart
-
-  @override
-  void initState() {
-    super.initState();
-    //TODO: Call getData() when the screen loads up.
+var a='';
+  var b='';
+  var f='';
+Future getData(String selectedCurrency)async {
+  CoinData c = CoinData(selectedCurrency);
+  await c.getCoinData();
+  setState(() {
+    a = c.rate;
+  });
+}
+  Future getData1(String selectedCurrency) async {
+    CoinData d = CoinData(selectedCurrency);
+    await d.getCoinData1();
+    setState(() {
+      b = d.rate;
+    });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('🤑 Coin Ticker'),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: <Widget>[
-          Padding(
-            padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
-            child: Card(
-              color: Colors.lightBlueAccent,
-              elevation: 5.0,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(10.0),
-              ),
-              child: Padding(
-                padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 28.0),
-                child: Text(
-                  //TODO: Update the Text Widget with the live bitcoin data here.
-                  '1 BTC = ? USD',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    color: Colors.white,
+    Future getData2(String selectedCurrency) async {
+      CoinData e = CoinData(selectedCurrency);
+      await e.getCoinData2();
+      setState(() {
+        f = e.rate;
+      });
+    }
+    //TODO: Create a method here called getData() to get the coin data from coin_data.dart
+
+    @override
+    void initState() {
+      super.initState();
+      getData1(selectedCurrency);
+      getData(selectedCurrency);
+      getData2(selectedCurrency);
+    }
+
+
+    @override
+    Widget build(BuildContext context) {
+      return Scaffold(
+        appBar: AppBar(
+          title: Text('🤑 Coin Ticker'),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+              child: Card(
+                color: Colors.lightBlueAccent,
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 28.0),
+                  child: Text(
+                    //TODO: Update the Text Widget with the live bitcoin data here.
+                    '1 BTC = $a $selectedCurrency',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
                   ),
                 ),
+
+
               ),
             ),
-          ),
-          Container(
-            height: 150.0,
-            alignment: Alignment.center,
-            padding: EdgeInsets.only(bottom: 30.0),
-            color: Colors.lightBlue,
-            child: Platform.isIOS ? iOSPicker() : androidDropdown(),
-          ),
-        ],
-      ),
-    );
+            Padding(
+              padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+              child: Card(
+                color: Colors.lightBlueAccent,
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 28.0),
+                  child: Text(
+                    //TODO: Update the Text Widget with the live bitcoin data here.
+                    '1 ETH = $b $selectedCurrency',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+
+
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.fromLTRB(18.0, 18.0, 18.0, 0),
+              child: Card(
+                color: Colors.lightBlueAccent,
+                elevation: 5.0,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.symmetric(
+                      vertical: 15.0, horizontal: 28.0),
+                  child: Text(
+                    //TODO: Update the Text Widget with the live bitcoin data here.
+                    '1 LTC = $f $selectedCurrency',
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+
+
+              ),
+            ),
+            Container(
+              height: 150.0,
+              alignment: Alignment.center,
+              padding: EdgeInsets.only(bottom: 30.0),
+              color: Colors.lightBlue,
+              child: Platform.isIOS ? iOSPicker() : androidDropdown(),
+            ),
+
+          ],
+        ),
+      );
+    }
   }
-}
